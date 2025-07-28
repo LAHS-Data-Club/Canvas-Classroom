6109933d7c292f543b3145974fa82302cf23cef9
netlle <nettlepluff@gmail.com>
2025-07-27 21:23:51 -0700

    silly me

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { asyncHandler } from './src/helper/helper';
 import { authRouter } from './src/routers/authRouter';
 import { 
@@ -10,8 +12,9 @@ import { getCourseModules, getCourseAssignment, getCourses } from './src/functio
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
-const TOKEN = '10497~B6tWRntMNzDCmWhW7fAAQNaFThETLaTwV8G6JXKRuHfYhWaxft73TDrvHtNVeUBY';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../client', 'dist')));
 
 // TODO: planner, not sure how to do that
 // TODO: i forgot about the whole rel=last scenario ; maybe use that instead we'll see
@@ -23,7 +26,7 @@ app.use('/', authRouter);
 app.get('/api/courses', 
   asyncHandler(async (req, res) => {
     getCourses(
-      TOKEN, 
+      req.cookies.canvas_token, 
       req.cookies.refresh_token
     ).then((data) => res.json(data));
 }));
@@ -31,7 +34,7 @@ app.get('/api/courses',
 app.get('/api/courses/:courseId/modules', 
   asyncHandler(async (req, res) => {
     getCourseModules(
-      TOKEN,
+      req.cookies.canvas_token,
       req.cookies.refresh_token,
       req.params.courseId,
       req.query.origin as string
@@ -41,7 +44,7 @@ app.get('/api/courses/:courseId/modules',
 app.get('/api/courses/:courseId/assignments', 
   asyncHandler(async (req, res) => {
     getCourseAssignment(
-      TOKEN,
+      req.cookies.canvas_token,
       req.cookies.refresh_token,
       req.params.courseId,
       req.query.page as string,
@@ -53,11 +56,15 @@ app.get('/api/courses/:courseId/assignments',
 app.get('/api/courses/:courseId/modules/:moduleId/items', 
   asyncHandler(async (req, res) => {
     getCanvasModulePage(
-      TOKEN,
+      req.cookies.canvas_token,
       req.params.courseId,
       req.params.moduleId,
       req.query.page as string
     ).then((data) => res.json(data));
+}));
+
+app.get('/{*any}', asyncHandler(async (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));  
 }));
 
 const PORT = 8000;
